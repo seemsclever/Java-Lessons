442cbc6656b44dbbae1961dcbab935fdf251dc67
seemsclever <seemsclever@mail.ru>
2025-01-23 19:13:31 +0500

    Решение задачи: На вход поступает число, вывести отзеркаленное число. Реализация через строки

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 414431;
-        int number2 = number;
-        int reversedNumber = 0;
-        int amountOfDigits = 0;
-        System.out.println(number);
-        while (number != 0){
-            amountOfDigits++;
-            number /= 10;
+        int number = 123456789;
+        String stringNumber = String.valueOf(number);
+        char[] arrayOfDigits = stringNumber.toCharArray();
+        String reversedString = "";
+        System.out.println(stringNumber);
+        for (int i = arrayOfDigits.length - 1; i >= 0; i--) {
+            reversedString += arrayOfDigits[i];
         }
-        double i = Math.pow(10, (amountOfDigits - 1));
-        while (number2 != 0){
-            double digit = number2 % 10;
-            reversedNumber += digit * i;
-            i/=10;
-            number2 /= 10;
-        }
-        System.out.println(reversedNumber);
+        System.out.println(reversedString);
     }
 }
