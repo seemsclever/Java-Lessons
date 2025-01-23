1c288478869edc6141f3287f40f1775c611a8f2d
seemsclever <seemsclever@mail.ru>
2025-01-23 18:03:39 +0500

    Решение задачи: На вход поступает число, вывести отзеркаленное число

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Привет, мир!\n");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        int number = 414431;
+        int number2 = number;
+        int reversedNumber = 0;
+        int amountOfDigits = 0;
+        System.out.println(number);
+        while (number != 0){
+            amountOfDigits++;
+            number /= 10;
         }
+        double i = Math.pow(10, (amountOfDigits - 1));
+        while (number2 != 0){
+            double digit = number2 % 10;
+            reversedNumber += digit * i;
+            i/=10;
+            number2 /= 10;
+        }
+        System.out.println(reversedNumber);
     }
 }
