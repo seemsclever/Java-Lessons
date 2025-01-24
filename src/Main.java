3095c8414b60d58f1a86c3bb13d7f5fa67262671
seemsclever <seemsclever@mail.ru>
2025-01-24 23:13:20 +0500

    Решение задачи: На вход дан массив интов. Найти максимально большую сумму пар чисел. (ФИКС)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {3, 45, 2, 50, 46, 1, 42, 2, 6, 70, 11, 3};
+        int[] arr = {71, 70, 0, 0, 71};
         int maxValue = 0;
         int maxValue2 = 0;
         for (int i : arr) {
             if (i >= maxValue) {
                 maxValue2 = maxValue;
                 maxValue = i;
+            }
+            if (i > maxValue2 && i < maxValue){
+                maxValue2 = i;
             }
         }
         int sum = maxValue + maxValue2;
