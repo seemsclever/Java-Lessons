050ec13a3329ae634338feb3722446343b5ace7c
seemsclever <seemsclever@mail.ru>
2025-01-24 21:27:15 +0500

    Решение задачи: На вход дана строка. Проверить, является ли строка палиндромом

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 123456789;
-        String stringNumber = String.valueOf(number);
-        char[] arrayOfDigits = stringNumber.toCharArray();
-        String reversedString = "";
-        System.out.println(stringNumber);
-        for (int i = arrayOfDigits.length - 1; i >= 0; i--) {
-            reversedString += arrayOfDigits[i];
+        String str = "adsswwssda";
+        char[] letters = str.toCharArray();
+        boolean isPalindrom = true;
+        for (int i = 0; i < letters.length/2; i++) {
+            if (letters[i] != letters[letters.length - i - 1]) {
+                isPalindrom = false;
+                break;
+            }
         }
-        System.out.println(reversedString);
+        System.out.println("isPalindrom = " + isPalindrom);
     }
 }
