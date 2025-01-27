88f83176bd93ff9ae28f46eb2e54f378ffdf7df8
seemsclever <seemsclever@mail.ru>
2025-01-27 19:15:24 +0500

    Решение задачи: Дан массив из N положительных чисел. Найти в нем несколько чисел, идущих подряд, так, чтобы их сумма была больше числа K , а суммирующихся элементов бы как можно меньше. (Решение со сложностью O(n^2))

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,37 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {71, 70, 0, 0, 71};
-        int maxValue = 0;
-        int maxValue2 = 0;
-        for (int i : arr) {
-            if (i >= maxValue) {
-                maxValue2 = maxValue;
-                maxValue = i;
+        int[] arr = {1, 2, 6, 3, 2, 5};
+        //           0  1  2  3  4  5
+        System.out.println(Arrays.toString(arr));
+        int k = 6;
+        int index = 0;
+        int minElements = 5;
+        int leftIndex = 0;
+        while (leftIndex <= arr.length - 1){
+            int sum = 0;
+            int amounthOfElements = 0;
+            int rightIndex = leftIndex;
+            while (sum <= k && rightIndex <= arr.length - 1){
+                sum += arr[rightIndex];
+                rightIndex++;
+                amounthOfElements++;
             }
-            if (i > maxValue2 && i < maxValue){
-                maxValue2 = i;
+            if (sum > k && amounthOfElements < minElements){
+                minElements = amounthOfElements;
+                index = leftIndex;
             }
+            System.out.printf("arr[%d] = %d: Используется элементов - %d, Сумма - %d%n", leftIndex, arr[leftIndex], amounthOfElements, sum);
+            leftIndex++;
         }
-        int sum = maxValue + maxValue2;
-        System.out.println("sum = " + sum);
+        System.out.printf("Ответ:%nЭлементы ");
+        for (int i = index; i < index + minElements; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+        System.out.printf("под индексами ");
+        for (int i = index; i < index + minElements; i++) {
+            System.out.printf("%d ", i);
+        }
     }
 }
