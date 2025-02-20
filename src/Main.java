4bff7e53583349e7598e3a8793e2736d9e8e1063
seemsclever <seemsclever@mail.ru>
2025-02-20 21:00:07 +0500

    Решение задачи: На вход дан массив интов. Найти количество вхождений каждого числа

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,21 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {0, 414, 24, 5151, 2522, 2};
-        System.out.println(Arrays.toString(getNumbersWithMaxAndMinDigits(array)));
+        int[] array = {1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 3, 3};
+        System.out.println(getNumbersWithMaxAndMinRepeats(array));
     }
 
-    public static int[] getNumbersWithMaxAndMinDigits(int[] arr){
-        int min = 0;
-        int max = 0;
-        int numOfDigits = 0;
-        int numberWithMin = 0;
-        int numberWithMax = 0;
-        for (int i : arr) {
-            numOfDigits = countDigits(i);
-            if (numOfDigits >= max){
-                max = numOfDigits;
-                numberWithMax = i;
-            } else if (numOfDigits <= min) {
-                min = numOfDigits;
-                numberWithMin = i;
-            }
+    public static Map<Integer, Integer> getNumbersWithMaxAndMinRepeats(int[] arr){
+        HashMap<Integer, Integer> integerIntegerHashMap = new HashMap<>();
+        int counter;
+        for (int key : arr) {
+            counter = integerIntegerHashMap.getOrDefault(key, 0);
+            counter++;
+            integerIntegerHashMap.put(key, counter);
         }
-        int[] newArr = new int[2];
-        newArr[0] = numberWithMin;
-        newArr[1] = numberWithMax;
-        return newArr;
-    }
-
-    public static int countDigits(int a){
-        int numOfDigits = 0;
-        while (a > 0){
-            numOfDigits++;
-            a = a /10;
-        }
-        return numOfDigits;
+        return integerIntegerHashMap;
     }
 }
